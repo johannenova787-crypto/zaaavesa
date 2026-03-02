33a51349080f7ea7418e73a92114d170b4aa0fbe
johannenova787-crypto <johannenova787@gmail.com>
2026-03-02 15:04:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 
+function setTimeMode() {
+  const hour = new Date().getHours();
+  const body = document.body;
+
+  body.classList.remove("day", "sunset", "night");
+
+  if (hour >= 6 && hour < 18) {
+    body.classList.add("day");
+  } else if (hour >= 18 && hour < 21) {
+    body.classList.add("sunset");
+  } else {
+    body.classList.add("night");
+  }
+}
+
+setTimeMode();
+
+/* обновлять каждые 10 минут */
+setInterval(setTimeMode, 600000);
